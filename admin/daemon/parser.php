a77a957c3f00f4dca3e26ea0007358d42101cbf8
locus43 <jan.sommer01@gmail.com>
2021-09-19 15:14:12 +0200

    automated newsletter function

--- admin/daemon/parser.php
+++ admin/daemon/parser.php
@@ -1,2 +1,20 @@
 <?php
+//file to get data from json and parse it into right mail format
 //ToDo: implement functions to parse text and send mail
+
+require_once("../../include/db.php");
+parser::getEvents();
+class parser{
+    public function getEvents(){
+        $jsonFile =  file_get_contents("../../include/tmp/tmp.json");
+        $data = json_decode($jsonFile, true);
+        parser::getUser("gruppen");
+
+
+    }private function getUser($topic){
+        $query = "select * from topics inner join newsletter on topics.uid = newsletter.id having " . $topic . " = '1'";
+        $result = db::getInstance()->get_result($query);
+        var_dump($result);
+        echo $result["email"];
+}
+}
